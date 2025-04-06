37ca098dc2afa11d142ac98654ab997f26192d18
Wrong-o <otto@rongedal.se>
2025-04-07 00:11:01 +0200

    attemt 22

--- backend/security.py
+++ backend/security.py
@@ -1,9 +1,8 @@
 import base64
 from datetime import UTC, datetime, timedelta, timezone
 from random import SystemRandom
-from typing import Annotated, Optional
+from typing import Annotated
 from uuid import uuid4, UUID
-import os
 
 from api.v1.core.models import User, Token
 from db_setup import get_db
@@ -14,16 +13,8 @@ from passlib.context import CryptContext
 from pydantic import ValidationError
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from dotenv import load_dotenv
-from jose import JWTError, jwt
 
-load_dotenv()
-
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/v1/auth/token")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 DEFAULT_ENTROPY = 32
@@ -108,34 +99,3 @@ def get_current_token(
     """
     token = verify_token_access(token_str=token, db=db)
     return token
-
-def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
-
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-def verify_token(token: str, credentials_exception):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email: str = payload.get("sub")
-        if email is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-    return email
-
-def get_current_user_optional(token: Optional[str] = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> Optional[User]:
-    if token is None:
-        return None
-    try:
-        return get_current_user(token, db)
-    except HTTPException:
-        return None
